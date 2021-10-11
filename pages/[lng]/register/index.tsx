d4ad4ce1a4091bb25bd47658a351fcc15a4091a1
Galih  Dwi Putra <galihdwiputra.gdp@gmail.com>
2021-10-11 16:50:17 +0700

    update form auth

--- pages/[lng]/register/index.tsx
+++ pages/[lng]/register/index.tsx
@@ -15,6 +15,7 @@ import {
   useI18n
 } from '@sirclo/nexus'
 import { parseCookies } from 'lib/parseCookies'
+import dynamic from "next/dynamic";
 import redirectIfAuthenticated from 'lib/redirectIfAuthenticated'
 import { useBrand } from 'lib/utils/useBrand'
 import { useGoogleAuth } from 'lib/utils/useGoogleAuth'
@@ -44,6 +45,11 @@ const classesRegister = {
   datePickerCalendarClassName: "date-picker__calendar"
 }
 
+const Widget = dynamic(
+  () => import("@sirclo/nexus").then((mod) => mod.Widget),
+  { ssr: false }
+);
+
 const RegisterPage: FC<any> = ({
   lng,
   lngDict,
@@ -53,6 +59,7 @@ const RegisterPage: FC<any> = ({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const i18n: any = useI18n()
   const [isVerified, setIsVerified] = useState<boolean>(false)
+  
 
   const linksBreadcrumb = [
     `${i18n.t("home.title")}`,
@@ -66,80 +73,87 @@ const RegisterPage: FC<any> = ({
       lngDict={lngDict}
       brand={brand}
     >
-      <Breadcrumb
+      {/* <Breadcrumb
         title={i18n.t("register.title")}
         links={linksBreadcrumb}
         lng={lng}
-      />
+      /> */}
       <section>
-        <div className="container">
-          <div className="register-page-container">
-            <div className="row mb-5">
-              <div className="col-12 col-sm-12 col-lg-6">
-                <h3 className="login-page-title">
-                  {i18n.t("register.newAccount")}
-                </h3>
-                <span className="login-page-subtitle">
-                  {i18n.t("register.promo")}
-                </span>
-                {(hasGoogleAuth || hasFacebookAuth) &&
-                  <>
-                    <div className="login-page-withGoogle register">
-                      <SingleSignOn
-                        className="login-page-withGoogleButton"
-                        buttonText={i18n.t("register.sso")}
-                        onErrorMsg={(msg: string) => toast.error(msg)}
-                        loadingComponent={
-                          <div className="quickdetail__overlay">
-                            <LoaderPages />
-                          </div>
+        {/* <div className="container"> */}
+          <div className="row">
+            <div className="col-12 col-sm-12 col-lg-4">
+              <Widget pos="main-content-2" widgetClassName="widget-login" />
+            </div>
+            <div className="col-12 col-sm-12 col-lg-8">
+              <div className="register-page-container">
+                <div className="row mb-5">
+                  <div className="col-12 col-sm-12 col-lg-6">
+                    <h3 className="login-page-title">
+                      {i18n.t("register.newAccount")}
+                    </h3>
+                    <span className="login-page-subtitle">
+                      {i18n.t("register.promo")}
+                    </span>
+                    {(hasGoogleAuth || hasFacebookAuth) &&
+                      <>
+                        <div className="login-page-withGoogle register">
+                          <SingleSignOn
+                            className="login-page-withGoogleButton"
+                            buttonText={i18n.t("register.sso")}
+                            onErrorMsg={(msg: string) => toast.error(msg)}
+                            loadingComponent={
+                              <div className="quickdetail__overlay">
+                                <LoaderPages />
+                              </div>
+                            }
+                          />
+                        </div>
+                        <label className="login-page-orTitle"><span>{i18n.t("testimonials.or")}</span></label>
+                      </>
+                    }
+                  </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-12 col-md-12">
+                      <Register
+                        classes={classesRegister}
+                        withHeaderLabel={true}
+                        onErrorMsg={(msg) => toast.error(msg)}
+                        onSuccessMsg={(msg) => toast.success(msg)}
+                        redirectPage={() =>
+                          Router.push(`/[lng]/login`, `/${lng}/login`)
                         }
+                        passwordViewIcon={
+                          <FontAwesomeIcon className="icon-password" icon={faEye} />
+                        }
+                        passwordHideIcon={
+                          <FontAwesomeIcon
+                            className="icon-password"
+                            icon={faEyeSlash}
+                          />
+                        }
+                        passwordUnfulfilledCriteriaIcon={
+                          <FontAwesomeIcon icon={faCheckCircle} height="1.25em" />
+                        }
+                        passwordFulfilledCriteriaIcon={
+                          <FontAwesomeIcon icon={faCheckCircle} height="1.25em" />
+                        }
+                        withVerification={true}
+                        isVerified={isVerified}
+                        verificationComponent={
+                          <ReCAPTCHA
+                            sitekey={process.env.NEXT_PUBLIC_SITEKEY_RECAPTCHA}
+                            onChange={() => setIsVerified(true)}
+                          />
+                        }
+                        loadingComponent={<Loader color="text-light" />}
                       />
                     </div>
-                    <label className="login-page-orTitle"><span>{i18n.t("testimonials.or")}</span></label>
-                  </>
-                }
+                  </div>
               </div>
             </div>
-            <div className="row mb-3">
-              <div className="col-12 col-md-12">
-                <Register
-                  classes={classesRegister}
-                  withHeaderLabel={true}
-                  onErrorMsg={(msg) => toast.error(msg)}
-                  onSuccessMsg={(msg) => toast.success(msg)}
-                  redirectPage={() =>
-                    Router.push(`/[lng]/login`, `/${lng}/login`)
-                  }
-                  passwordViewIcon={
-                    <FontAwesomeIcon className="icon-password" icon={faEye} />
-                  }
-                  passwordHideIcon={
-                    <FontAwesomeIcon
-                      className="icon-password"
-                      icon={faEyeSlash}
-                    />
-                  }
-                  passwordUnfulfilledCriteriaIcon={
-                    <FontAwesomeIcon icon={faCheckCircle} height="1.25em" />
-                  }
-                  passwordFulfilledCriteriaIcon={
-                    <FontAwesomeIcon icon={faCheckCircle} height="1.25em" />
-                  }
-                  withVerification={true}
-                  isVerified={isVerified}
-                  verificationComponent={
-                    <ReCAPTCHA
-                      sitekey={process.env.NEXT_PUBLIC_SITEKEY_RECAPTCHA}
-                      onChange={() => setIsVerified(true)}
-                    />
-                  }
-                  loadingComponent={<Loader color="text-light" />}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+          </div> 
+        {/* </div> */}
       </section>
     </Layout>
   )
