03f1151480d1e030d55aabd3dcab7a50afd857e0
Galih  Dwi Putra <galihdwiputra.gdp@gmail.com>
2021-10-15 08:46:52 +0700

    update dompet

--- components/Breadcrumb/Breadcrumb.tsx
+++ components/Breadcrumb/Breadcrumb.tsx
@@ -15,12 +15,15 @@ const Breadcrumb: FC<any> = ({
   lng
 }) => {
   return (
+    <div>
     <div
       className={`section-breadcrumb ${withImage ? "section-breadcrumb__image" : ""}`}
       style={{ backgroundImage: `url(${withImage})` }}
     >
-      <h1 className="section-breadcrumb__title">{title}</h1>
-      <nav aria-label="breadcrumb">
+      <h1 className="section-breadcrumb__title">{title}</h1>  
+    </div>
+    <div className="section-breadcrumb" >
+        <nav aria-label="breadcrumb">
         <ol className="breadcrumb breadcrumb-merlin">
           {
             links.map((el, idx) => {
@@ -42,6 +45,7 @@ const Breadcrumb: FC<any> = ({
           }
         </ol>
       </nav>
+      </div>
     </div>
   )
 }
