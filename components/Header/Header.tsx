8dadababd640d5d5349f8764deae1896852b11bd
fl <rizalpratama10010@gmail.com>
2021-10-18 10:26:48 +0700

    update header color

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -53,7 +53,7 @@ const Header = ({ lng }) => {
     paddingRight: "0",
     paddingLeft: "200px",
     justifyContent: "left",
-    backgroundColor: "#3C4144"
+    backgroundColor: "#2C3E50"
   };
 
   return (
